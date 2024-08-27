830cfaacb74490d4b69224e476d2dbd1fbb73759
frobi <frobi.webdev@outlook.com>
2024-08-27 21:07:14 +0930

    create-a-component-for-managing-personal-details-input-via-props

---
+++ src/PersonalDetails.jsx
@@ -0,0 +1,52 @@
+import './personal-details.css';
+import { useState } from 'react';
+
+function TextInput({ label, name, value, onChange}) {
+    return (
+        <div className="input-group">
+            <label htmlFor={name}>{label}</label>
+            <input
+                className='text-input'
+                type="text"
+                id={name}
+                name={name}
+                value={value}
+                onChange={onChange}
+            />
+        </div>
+    )
+}
+
+export default function personalDetailsForm({ fullName, email, phoneNumber, address, onInputChange }) {
+    return(
+        <>
+            <section className="personal-details-form">
+                <h1>Personal Details</h1>
+                <TextInput
+                    label="Full Name"
+                    name="fullName"
+                    value={fullName}
+                    onChange={(event) => onInputChange("fullName", event.target.value)}
+                />
+                <TextInput
+                    label="Email"
+                    name="email"
+                    value={email}
+                    onChange={(event) => onInputChange("email", event.target.value)}
+                />
+                <TextInput
+                    label="Phone Number"
+                    name="phoneNumber"
+                    value={phoneNumber}
+                    onChange={(event) => onInputChange("phoneNumber", event.target.value)}
+                />
+                <TextInput
+                    label="Address"
+                    name="address"
+                    value={address}
+                    onChange={(event) => onInputChange("address", event.target.value)}
+                />
+            </section>
+        </>
+    )
+}
