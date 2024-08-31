0efa6712a70e85687b27b9afc1c02f533ab10df5
frobi <frobi.webdev@outlook.com>
2024-08-31 20:06:23 +0930

    display-icons-for-contact-details

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -1,13 +1,22 @@
 import './resume.css';
+import Icon from '@mdi/react';
+import { mdiEmail } from "@mdi/js";
+import { mdiPhone } from '@mdi/js';
+import { mdiMapMarker } from '@mdi/js';
 
 export default function Resume({fullName, email, phoneNumber, address}) {
     return (
         <div className="resume">
             <section className="personal-details">
-                <div className="full-name">{fullName}</div>
+                <div className="full-name">
+                    {fullName}
+                </div>
                 <div className="contact-info">
+                    {email && <Icon path={mdiEmail} size={1} />}
                     <div className="email">{email}</div>
+                    {phoneNumber && <Icon path={mdiPhone} size={1} />}
                     <div className="phone-number">{phoneNumber}</div>
+                    {address && <Icon path={mdiMapMarker} size={1} />}
                     <div className="address">{address}</div>
                 </div>
             </section>
