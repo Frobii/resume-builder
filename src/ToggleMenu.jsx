0cf65f5dd8576533e770e6f4f4fb68d6c5a4ccf5
frobi <frobi.webdev@outlook.com>
2024-09-01 16:00:23 +0930

    style-the-toggle-menu-component

--- src/ToggleMenu.jsx
+++ src/ToggleMenu.jsx
@@ -1,7 +1,10 @@
+import './toggle-menu.css'
 import { useState } from 'react';
 import Icon from '@mdi/react';
 import { mdiChevronDown } from '@mdi/js';
 import { mdiChevronUp } from '@mdi/js';
+import { mdiSchool } from '@mdi/js';
+import { mdiBriefcase } from '@mdi/js';
 
 export default function ToggleMenu({children, title}) {
     const [toggled, setToggled] = useState(false);
@@ -11,15 +14,21 @@ export default function ToggleMenu({children, title}) {
     }
 
     return(
-        <div className="toggle-container">
-            <h1>{title}</h1>
-            <button onClick={toggleMenu}>
-                {toggled ? 
-                    <Icon path={mdiChevronUp} size={1} /> :
-                    <Icon path={mdiChevronDown} size={1} /> 
-                }
-            </button>
-            {toggled && children}
-        </div>
+        <>
+            <div className="toggle-container">
+                <div className="toggle-heading">
+                    {title === "Education" && <Icon path={mdiSchool} size={1.5} />}
+                    {title === "Experience" && <Icon path={mdiBriefcase} size={1.5} />}
+                    <h1 className='title'>{title}</h1>
+                    <button className='toggle-button' onClick={toggleMenu}>
+                        {toggled ? 
+                            <Icon path={mdiChevronUp} size={1} /> :
+                            <Icon path={mdiChevronDown} size={1} /> 
+                        }
+                    </button>
+                </div>
+                {toggled && children}
+            </div>
+        </>
     )
 }
