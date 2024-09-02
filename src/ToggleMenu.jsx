7ef595fc1fa75da05627b198805509df8d5fb4c5
frobi <frobi.webdev@outlook.com>
2024-09-02 19:25:36 +0930

    remove-bottom-border-radius-when-toggled

--- src/ToggleMenu.jsx
+++ src/ToggleMenu.jsx
@@ -16,7 +16,13 @@ export default function ToggleMenu({children, title}) {
     return(
         <>
             <div className="toggle-container">
-                <div className="toggle-heading">
+                <div 
+                    className="toggle-heading"
+                    style = {{
+                        borderBottomRightRadius: toggled ? '0rem' : '0.7rem',
+                        borderBottomLeftRadius: toggled ? '0rem' : '0.7rem',
+                    }}
+                >
                     {title === "Education" && <Icon path={mdiSchool} size={1.5} />}
                     {title === "Experience" && <Icon path={mdiBriefcase} size={1.5} />}
                     <h1 className='title'>{title}</h1>
